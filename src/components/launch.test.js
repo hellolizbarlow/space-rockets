5516f4db4f0e81ea62dde68686511e0b5c9b4acb
Liz Barlow <hello@lizbarlow.com>
2022-03-22 10:00:19 +0000

    Fix unit tests

--- src/components/launch.test.js
+++ src/components/launch.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../utils/match-media.mock';
 import { render, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { formatDateTime } from '../utils/format-date';
